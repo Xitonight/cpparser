ce70fca948a4c7a1dd1e10ece809ed3383d5c106
Xitonight <xitonight@gmail.com>
2026-03-30 19:01:00 +0200

    chore: removed commented code

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -169,27 +169,6 @@ bool Lexer::match(char expected) {
 bool Lexer::isAtEnd() const { return current_ >= source_.length(); }
 
 void Lexer::throwLexingError(std::string_view msg) const {
-  // size_t lineStart = current_;
-  //
-  // while (lineStart > 0 && source_[lineStart - 1] != '\n')
-  //   lineStart--;
-  //
-  // size_t lineEnd = current_;
-  //
-  // while (lineEnd < source_.size() && source_[lineEnd] != '\n')
-  //   lineEnd++;
-  //
-  // std::string errorLine = source_.substr(lineStart, lineEnd - lineStart);
-  //
-  // std::cerr << std::endl
-  //           << Color::RED << "[line " << line_ << "] Error: " << msg
-  //           << Color::RESET << std::endl
-  //           << " " << line_ << " | " << errorLine << std::endl
-  //           << " " << std::string(std::to_string(line_).size(), ' ') << "  "
-  //           << std::string(column_ - 1, ' ') << Color::RED << "^"
-  //           << Color::RESET << std::endl;
-  //
-  // exit(1);
   throwLexingError(msg, current_ - 1);
 }
 
